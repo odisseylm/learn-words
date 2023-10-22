6fa5f81e573ba4ff4b00ad915bf122918d3721f0
vmelnykov <Volodymyr.Melnykov@gmail.com>
2023-10-22 05:42:31 +0300

    Fixing improved addKeyBinding()

--- src/main/java/com/mvv/gui/javafx/keys.kt
+++ src/main/java/com/mvv/gui/javafx/keys.kt
@@ -2,6 +2,7 @@ package com.mvv.gui.javafx
 
 import javafx.event.EventType
 import javafx.scene.Node
+import javafx.scene.Scene
 import javafx.scene.input.KeyCombination
 import javafx.scene.input.KeyEvent
 
@@ -20,19 +21,20 @@ fun <C: Node> addLocalKeyBindings(control: C, keyBindings: Map<KeyCombination, (
     addLocalKeyBindings(control, setOf(KeyEvent.KEY_RELEASED), keyBindings)
 
 fun <C: Node> addLocalKeyBindings(control: C, keyEventTypes: Set<EventType<KeyEvent>>, keyBindings: Map<KeyCombination, (C)-> Unit>) {
-    if (control.scene != null)
-        addLocalKeyBindingsImpl(control, keyEventTypes, keyBindings)
+    val scene: Scene? = control.scene
+    if (scene != null)
+        addLocalKeyBindingsImpl(control, scene, keyEventTypes, keyBindings)
     else
         // T O D O: would be nice to add protection from repeated call
         // We use some timeout to make sure that menu accelerators are already set up (to avoid repeating action's calls).
         control.sceneProperty().addListener { _,_, newScene ->
-            if (newScene != null) runLaterWithDelay(250L) { addLocalKeyBindingsImpl(control, keyEventTypes, keyBindings) } }
+            if (newScene != null) runLaterWithDelay(250L) { addLocalKeyBindingsImpl(control, newScene, keyEventTypes, keyBindings) } }
 }
 
-private fun <C: Node> addLocalKeyBindingsImpl(control: C, keyEventTypes: Set<EventType<KeyEvent>>, keyBindings: Map<KeyCombination, (C)-> Unit>) =
+private fun <C: Node> addLocalKeyBindingsImpl(control: C, scene: Scene, keyEventTypes: Set<EventType<KeyEvent>>, keyBindings: Map<KeyCombination, (C)-> Unit>) =
     keyEventTypes.forEach { keyEventType -> // KeyEvent.KEY_PRESSED, KeyEvent.KEY_TYPED, KeyEvent.KEY_RELEASED
 
-        val rootKeyCombinations: Set<KeyCombination> = control.scene.accelerators.keys
+        val rootKeyCombinations: Set<KeyCombination> = scene.accelerators.keys
         val notRegisteredYetKeyBindings = keyBindings.filterNot { it.key in rootKeyCombinations }
 
         control.addEventHandler(keyEventType) {
@@ -50,16 +52,17 @@ fun <C: Node> addGlobalKeyBindings(control: C, keyBindings: Iterable<KeyCombinat
     keyBindings.forEach { keyBinding -> addGlobalKeyBinding(control, keyBinding, action) }
 
 fun <C: Node> addGlobalKeyBindings(control: C, keyBindings: Map<KeyCombination, (C)-> Unit>) {
-    if (control.scene != null)
-        addGlobalKeyBindingsImpl(control, keyBindings)
+    val scene = control.scene
+    if (scene != null)
+        addGlobalKeyBindingsImpl(control, scene, keyBindings)
     else
         // T O D O: would be nice to add protection from repeated call
         control.sceneProperty().addListener { _,_, newScene ->
             // We use some timeout to make sure that menu accelerators are already set up,
             // however for global binding it is not so important since later binding just overwrites previous one
             // because bindings are just kept in simple map
-            if (newScene != null) runLaterWithDelay(50L) { addGlobalKeyBindingsImpl(control, keyBindings) } }
+            if (newScene != null) runLaterWithDelay(50L) { addGlobalKeyBindingsImpl(control, newScene, keyBindings) } }
 }
 
-private fun <C: Node> addGlobalKeyBindingsImpl(control: C, keyBindings: Map<KeyCombination, (C)-> Unit>) =
-    keyBindings.forEach { (keyBinding, action) -> control.scene.accelerators.putIfAbsent(keyBinding) { action(control) } }
+private fun <C: Node> addGlobalKeyBindingsImpl(control: C, scene: Scene, keyBindings: Map<KeyCombination, (C)-> Unit>) =
+    keyBindings.forEach { (keyBinding, action) -> scene.accelerators.putIfAbsent(keyBinding) { action(control) } }
