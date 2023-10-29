1813461b1d81790bc448538a325a2b0a88be98ca
vmelnykov <Volodymyr.Melnykov@gmail.com>
2023-10-29 05:04:00 +0200

    Tiny StopWatch improvements

--- src/main/java/com/mvv/gui/util/stopWatch.kt
+++ src/main/java/com/mvv/gui/util/stopWatch.kt
@@ -10,11 +10,28 @@ fun startStopWatch(): StopWatch = StopWatch().also { it.start() }
 //fun StopWatch.Companion.started(msg: String): StopWatch = StopWatch(msg).also { it.start() }
 
 
-fun StopWatch.logInfo(log: mu.KLogger, point: String) {
-    log.info { "${this.message} => $point - elapsed ${this.time}ms" }
+val StopWatch.timeString: String get() {
+    val timeMs = this.time
+    val timeNs = this.nanoTime
+    return when {
+        timeMs > 10   -> "${timeMs}ms"
+        timeMs > 0    -> "${ "%.2f".format(timeNs/1000_000.0) }ms"
+        timeNs > 1000 -> "${timeNs/1000L}mcs"
+        else          -> "${this.nanoTime}ns"
+    }
 }
 
-fun StopWatch.logInfo(log: mu.KLogger) {
-    log.info { "${this.message} => elapsed ${this.time}ms" }
-}
+
+fun StopWatch.logInfo(log: mu.KLogger, point: String) =
+    log.info { "${this.message} => $point - elapsed ${this.timeString}" }
+
+fun StopWatch.logInfo(log: mu.KLogger) =
+    log.info { "${this.message} => elapsed ${this.timeString}" }
+
+
+fun StopWatch.debugInfo(log: mu.KLogger, point: String) =
+    log.debug { "${this.message} => $point - elapsed ${this.timeString}ns" }
+
+fun StopWatch.debugInfo(log: mu.KLogger) =
+    log.debug { "${this.message} => elapsed ${this.timeString}ns" }
 
---
+++ src/test/java/com/mvv/gui/util/StopWatchTest.kt
@@ -0,0 +1,88 @@
+package com.mvv.gui.util
+
+import org.apache.commons.lang3.time.StopWatch
+import org.assertj.core.api.SoftAssertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.platform.commons.util.ReflectionUtils
+import java.lang.reflect.Field
+import java.time.Duration
+import kotlin.reflect.KClass
+
+
+class StopWatchTest {
+
+    //@Test
+    @DisplayName("timeString")
+    @RepeatedTest(2)
+    fun test_timeString() {
+
+        val a = SoftAssertions()
+
+        a.assertThat(
+            createStopWatch(Duration.ofSeconds(2)).timeString)
+            .isEqualTo("2000ms")
+
+        a.assertThat(
+            createStopWatch(Duration.ofMillis(500)).timeString)
+            .isEqualTo("500ms")
+
+        a.assertThat(
+            createStopWatch(Duration.ofMillis(10)).timeString)
+            .isEqualTo("10.00ms")
+
+        a.assertThat(
+            createStopWatch(Duration.ofMillis(9)).timeString)
+            .isEqualTo("9.00ms")
+        a.assertThat(
+            createStopWatch(Duration.ofMillis(5)).timeString)
+            .isEqualTo("5.00ms")
+
+        a.assertThat(
+            createStopWatch(Duration.ofMillis(2)).timeString)
+            .isEqualTo("2.00ms")
+        a.assertThat(
+            createStopWatch(Duration.ofNanos(1_456_852L)).timeString)
+            .isEqualTo("1.46ms")
+        a.assertThat(
+            createStopWatch(Duration.ofNanos(465_852L)).timeString)
+            .isEqualTo("465mcs")
+        a.assertThat(
+            createStopWatch(Duration.ofNanos(65_852L)).timeString)
+            .isEqualTo("65mcs")
+
+        a.assertThat(
+            createStopWatch(Duration.ofNanos(852L)).timeString)
+            .isEqualTo("852ns")
+
+        a.assertThat(
+            createStopWatch(Duration.ofNanos(52L)).timeString)
+            .isEqualTo("52ns")
+
+        a.assertAll()
+    }
+
+
+    private fun createStopWatch(duration: Duration): StopWatch {
+        val sw = startStopWatch()
+        sw.stop()
+
+        val startTimeMillisF: Field = StopWatch::class.findField("startTimeMillis")
+        val startTimeNanosF:  Field = StopWatch::class.findField("startTimeNanos")
+
+        val stopTimeMillisF: Field = StopWatch::class.findField("stopTimeMillis")
+        val stopTimeNanosF:  Field = StopWatch::class.findField("stopTimeNanos")
+
+        stopTimeMillisF.set(sw, (startTimeMillisF.get(sw)) as Long + duration.toMillis())
+        stopTimeNanosF .set(sw, (startTimeNanosF .get(sw)) as Long + duration.toNanos())
+
+        return sw
+    }
+}
+
+private fun KClass<*>.findField(name: String): Field = this.java.findField(name)
+
+private fun Class<*>.findField(name: String): Field {
+    return ReflectionUtils.findFields(this, { it.name == name }, ReflectionUtils.HierarchyTraversalMode.TOP_DOWN)
+        .firstOrNull()?.also { it.trySetAccessible() } ?: throw IllegalArgumentException("Field ${this.name}.${name} is not found.")
+}
